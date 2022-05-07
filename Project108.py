2bf5692917a220de816b6e9248dc30a3b1298fa5
AqeelGani <88658847+AqeelGani@users.noreply.github.com>
2022-05-07 17:38:49 +0530

    Update Project108.py

--- Project108.py
+++ Project108.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import plotly.figure_factory as ff
 
-df = pd.read_csv('csv files/Mobile_Rating.csv')
+df = pd.read_csv('Mobile_Rating.csv')
 rating = df['Avg Rating'].tolist()
 
 fig = ff.create_distplot([rating],['Average Rating'])
